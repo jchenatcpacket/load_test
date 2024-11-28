8c7d8eb6352f5ce8e5e9108abe8dcc13e4429f40
James Chen <jchen@cpacketnetworks.com>
2024-11-27 19:28:47 -0800

    piaf client success

--- client_piaf.ml
+++ client_piaf.ml
@@ -1,21 +1,36 @@
 open Piaf
 
-let get_sync env ~sw url =
-  print_endline "Sending request...";
-  match Client.Oneshot.get ~sw env (Uri.of_string url) with
-  | Ok response ->
-    if Status.is_successful response.status
-    then Body.to_string response.body
-    else
-      let message = Status.to_string response.status in
-      Error (`Msg message)
-  | Error e -> failwith (Error.to_string e)
+let hostname = "https://lin-jchen-01.int.cpacket.com"
+let host = "https://10.50.4.134"
+let api = "/api/epg_fr/known_udp_protocols/"
 
-let () =
-  Eio_main.run (fun env ->
-  Eio.Switch.run (fun sw ->
-  match get_sync env ~sw "https://example.com" with
-  | Ok body -> print_endline body
-  | Error error ->
-    let message = Error.to_string error in
-    prerr_endline ("Error: " ^ message)))
+let create_client env ~sw = 
+  let client_result = Client.create 
+    env 
+    ~sw 
+    ~config:{
+      Config.default with allow_insecure = true;
+    }
+    (Uri.of_string host)
+  in
+  match client_result with 
+  | Ok client -> client
+  | _ -> failwith " fail to init client"
+
+let headers = 
+  let user = Printf.sprintf "%s:%s" (Sys.getenv "USERNAME") (Sys.getenv "PASSWORD") in
+  [("Authorization", "Basic " ^ Base64.encode_exn user)]
+
+let _ =
+  Eio_main.run @@ fun env ->
+  Eio.Switch.run @@ fun sw ->
+  let client = create_client env ~sw in
+  let res_result = Client.get ~headers client api in
+  let res_body = 
+    match res_result with
+    | Ok res -> Body.to_string res.body
+    | Error error -> failwith ("Error: " ^ Error.to_string error)
+  in
+  match res_body with 
+  | Ok body_str -> Eio.traceln "%s" body_str;Client.shutdown client
+  | _ -> failwith "error parsing res";;
