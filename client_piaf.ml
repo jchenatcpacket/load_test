1486c8bd90fbe7ec59b42d4b521fecc40cf3323d
James Chen <jchen@cpacketnetworks.com>
2024-11-30 13:32:01 -0800

    update tasks

--- client_piaf.ml
+++ client_piaf.ml
@@ -1,11 +1,12 @@
-open Piaf
-
 let hostname = "https://lin-jchen-01.int.cpacket.com"
 let host = "https://10.50.4.134"
 let api = "/api/epg_fr/known_udp_protocols/"
 
+let url = host ^ api
+
 let create_client env ~sw = 
-  let client_result = Client.create 
+  let open Piaf in
+  let client_result = Client.create
     env 
     ~sw 
     ~config:{
@@ -15,27 +16,45 @@ let create_client env ~sw =
   in
   match client_result with 
   | Ok client -> client
-  | _ -> failwith " fail to init client"
+  | Error err -> failwith ("fail to init client: " ^ Error.to_string err)
+
+let parallel_count = Sys.getenv "REQUEST_NUMBER" |> int_of_string
 
 let headers = 
   let user = Printf.sprintf "%s:%s" (Sys.getenv "USERNAME") (Sys.getenv "PASSWORD") in
   [("Authorization", "Basic " ^ Base64.encode_exn user)]
 
-let api_request client = 
-  let res_result = Client.get ~headers client api in
-  let res_body = 
-  match res_result with
-  | Ok res -> Body.to_string res.body
-  | Error err -> Error.to_string err |> failwith
-  in
-  match res_body with 
-  | Ok body_str -> body_str
-  | Error err -> Error.to_string err |> failwith 
-
-let _ =
-  Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun sw ->
+let api_request env ~sw = 
+  Eio.traceln "starting request...";
   let client = create_client env ~sw in
-  let res_body_str = api_request client in
-  Eio.traceln "response body: %s\n" res_body_str;
-  Client.shutdown client;;
+  let res_result = Piaf.Client.get ~headers client api in
+  match res_result with
+  | Ok res -> Piaf.Status.to_code res.status |> Eio.traceln "resp status: %d"
+  | Error err -> failwith (Piaf.Error.to_string err);
+  Piaf.Client.shutdown client;;
+
+let api_request2 env ~sw =
+  Eio.traceln "starting request...";
+  match Piaf.Client.Oneshot.get 
+    ~config:{
+      Piaf.Config.default with allow_insecure = true;
+    }
+    ~headers:headers
+    ~sw
+    env
+    (Uri.of_string url)
+  with
+  | Ok res -> Piaf.Status.to_code res.status |> Eio.traceln "resp status: %d"
+  | Error err -> failwith ("error resp: " ^ Piaf.Error.to_string err);;
+
+let main env =
+  let domain_mgr = Eio.Stdenv.domain_mgr env in
+  let clock = Eio.Stdenv.clock env in
+  Eio.Switch.run @@ fun sw ->
+    let pool = Eio.Executor_pool.create ~sw domain_mgr ~domain_count:(Domain.recommended_domain_count ()) in
+    let task = Eio.Executor_pool.submit_exn pool ~weight:0.01 (fun () -> api_request2) in
+    let tasks = List.init parallel_count (fun _ -> fun () -> task env ~sw) in
+    (* Helper.wait_until clock (); *)
+    Eio.Fiber.all tasks;;
+
+let _ = Eio_main.run @@ fun env -> main env;;
